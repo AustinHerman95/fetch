9bb6e0139a1bd12ddbead1b1a18b8a271ecd74fe
AustinHerman95 <austin.herman95@yahoo.com>
2016-05-15 23:29:48 -0400

    gets options, has isRunning method
    Now receives flash/sensitivity options to send to the listener service and has an isRunning static function

--- ScreenLockService.java
+++ ScreenLockService.java
@@ -1,9 +1,10 @@
-package com.example.[USER].fetch1;
+package com.example.overlordsupreme.fetch1;
 
 import android.app.IntentService;
 import android.app.KeyguardManager;
 import android.content.Intent;
 import android.content.Context;
+import android.os.Bundle;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -12,27 +13,31 @@ import android.content.Context;
  * TODO: Customize class - update intent actions, extra parameters and static
  * helper methods.
  */
- 
- //Checks if the screen is locked. This should always be running
 public class ScreenLockService extends IntentService {
+
+    public static final String TAG = "ScreenLockService";
+    private boolean flash;
+    private int sensitivity;
+    static private boolean ScreenLockRunning;
 
     public ScreenLockService() {
         super("ScreenLockService");
     }
 
-    public static final String TAG = "ScreenLockService";
-
     @Override
     protected void onHandleIntent(Intent intent) {
 
-        //Want this thread as light as possible since it will always be running
+        ScreenLockRunning = true;
+        Bundle extra = intent.getExtras();
+        flash = extra.getBoolean("FLASH");
+        sensitivity = extra.getInt("SENSITIVITY");
+
         new Thread(
                 new Runnable() {
                     @Override
                     public void run() {
-                        //Not set on this loop. Still testing things but this might be uneccesary. 
+
                         while(!Thread.interrupted()) {
-                            //simple way to get if the screen is locked
                             KeyguardManager myKeyManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
                             if (myKeyManager.inKeyguardRestrictedInputMode()) {
                                 startListener();
@@ -46,21 +51,25 @@ public class ScreenLockService extends IntentService {
         ).start();
     }
 
-    //Start the listener service if not already running
     protected void startListener(){
         Intent listenerIntent = new Intent(this, Listener.class);
         if(!Listener.isListening()) {
             listenerIntent.setAction("com.example.fetch1.Listener");
             listenerIntent.addCategory(TAG);
+            listenerIntent.putExtra("FLASH", flash);
+            listenerIntent.putExtra("SENSITIVITY", sensitivity);
             startService(listenerIntent);
         }
     }
-    //Stop the listener service if already running. This deals with how we stop listening and might get reworked
     protected void stopListener(){
         Intent listenerIntent = new Intent(this, Listener.class);
         if(Listener.isListening()) {
             listenerIntent.addCategory(TAG);
             stopService(listenerIntent);
         }
+    }
+
+    static public boolean isRunning(){
+        return ScreenLockRunning;
     }
 }
