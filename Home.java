173e0bfbb4431293cae6eeb4bfaf4b4f5e95db65
AustinHerman95 <austin.herman95@yahoo.com>
2016-05-16 14:58:41 -0400

    Added seekbar and textView

--- Home.java
+++ Home.java
@@ -1,7 +1,115 @@
 package com.example.overlordsupreme.fetch1;
 
 import android.app.Activity;
+import android.content.Intepackage com.example.overlordsupreme.fetch1;
+
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        
+        //set up a new intent with default flash/sensitivity settings
+        Intent intent = new Intent(this, ScreenLockService.class);
+        intent.putExtra("FLASH", true);
+        intent.putExtra("SENSITIVITY", 0);
+        startService(intent);
+
+        //set up the seekbar and the progress text
+        SeekBar seekbar;
+        seekbar = (SeekBar) findViewById(R.id.Sensitivity);
+        TextView sensitivityText;
+        sensitivityText = (TextView) findViewById(R.id.SensitivityText);
+        sensitivityText.setText("Sensitivity:" + seekbar.getProgress());
+
+        //write what happens when the user changes the seek bar
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int finalProgress = 0;
+
+            //when the user releases the slider
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                finalProgress = progress;
+                //Start screenLockService with new sensitivity setting
+                Intent SLS = new Intent(ScreenLockService.class.getName());
+                SLS.putExtra("SENSITIVITY", finalProgress);
+                Home.this.startService(SLS);
+            }
+            //the next two should probably have some implementation, still figuring out what to put there
+            //when the user has control of the slider
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+            //when the user stops controlling the slider
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    public void onHomeBoxChecked(View view){
+        //Get if the box was checked or unchecked
+        boolean checked = ((CheckBox) view).isChecked();
+
+        //Get which box was clicked (flash or on/off)
+        switch(view.getId()) {
+            //the ON/OFF check box - works I think, doesn't throw any exceptions
+            case R.id.appOnBox:
+                if (checked){
+                    //Start the Screen Lock Service
+                    Intent lockService = new Intent(this, ScreenLockService.class);
+                    if(!ScreenLockService.isRunning()){
+                        lockService.setAction("com.example.fetch1.ScreenLockService");
+                        startService(lockService);
+                    }
+                }
+                else{
+                    //Stop the Screen Lock Service
+                    Intent lockService = new Intent(this, ScreenLockService.class);
+                    if(ScreenLockService.isRunning()){
+                        stopService(lockService);
+                    }
+                }
+                break;
+            //the flash light check box *does NOT work - check issues on github
+            case R.id.flashBox:
+                if (checked){
+                    //set up new intent with flash
+                    Intent SLS = new Intent(ScreenLockService.class.getName());
+                    SLS.putExtra("FLASH", true);
+                    Home.this.startService(SLS);
+                }
+                else {
+                    //set up new intent with no flash
+                    Intent SLS = new Intent(ScreenLockService.class.getName());
+                    SLS.putExtra("FLASH", false);
+                    Home.this.startService(SLS);
+                }
+                break;
+        }
+    }
+    
+    public void onInfoPageClicked(View view){
+        Intent intent = new Intent(this, InfoPage.class);
+        startActivity(intent);
+        //start infoPage activity
+    }
+}
+nt;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
