311c5c8cd2545129e808e1ff83c8b82be1e2e0bb
AustinHerman95 <austin.herman95@yahoo.com>
2016-06-01 19:17:53 -0400

    Decluttered stuff

--- Home.java
+++ Home.java
@@ -1,50 +1,65 @@
 package com.example.overlordsupreme.fetch1;
 
 import android.app.Activity;
-import android.content.Intepackage com.example.overlordsupreme.fetch1;
-
-import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Process;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import java.util.Iterator;
+import java.util.List;
 
 public class Home extends Activity {
+
+    public static final String TAG = "HomeActivity";
+    public static boolean flash;
+    public static int sensitivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        
+
+        flash = true;
+        sensitivity = 0;
+
         //set up a new intent with default flash/sensitivity settings
         Intent intent = new Intent(this, ScreenLockService.class);
-        intent.putExtra("FLASH", true);
-        intent.putExtra("SENSITIVITY", 0);
+        intent.putExtra("FLASH", flash);
+        intent.putExtra("SENSITIVITY", sensitivity);
         startService(intent);
 
         //set up the seekbar and the progress text
         SeekBar seekbar;
         seekbar = (SeekBar) findViewById(R.id.Sensitivity);
-        TextView sensitivityText;
+        /*TextView sensitivityText;
         sensitivityText = (TextView) findViewById(R.id.SensitivityText);
-        sensitivityText.setText("Sensitivity:" + seekbar.getProgress());
+        sensitivityText.setText("Sensitivity:" + seekbar.getProgress());*/
 
         //write what happens when the user changes the seek bar
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int finalProgress = 0;
+
 
             //when the user releases the slider
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                finalProgress = progress;
+                sensitivity = progress;
+
+                TextView sensitivityText;
+                sensitivityText = (TextView) findViewById(R.id.SensitivityText);
+                sensitivityText.setText(getString(R.string.sensitivity, progress));
+
                 //Start screenLockService with new sensitivity setting
-                Intent SLS = new Intent(ScreenLockService.class.getName());
-                SLS.putExtra("SENSITIVITY", finalProgress);
-                Home.this.startService(SLS);
+                Context context = getApplicationContext();
+                Intent SLS = new Intent(context, ScreenLockService.class);
+                SLS.putExtra("SENSITIVITY", sensitivity);
+                context.startService(SLS);
             }
             //the next two should probably have some implementation, still figuring out what to put there
             //when the user has control of the slider
@@ -70,176 +85,83 @@ public class Home extends Activity {
             //the ON/OFF check box - works I think, doesn't throw any exceptions
             case R.id.appOnBox:
                 if (checked){
+
+                    ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+                    for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+                        if ("com.example.ScreenLockService".equals(service.service.getClassName())) {
+
+                        }
+                    }
+
+
                     //Start the Screen Lock Service
-                    Intent lockService = new Intent(this, ScreenLockService.class);
+                    Intent SLS = new Intent(this, ScreenLockService.class);
                     if(!ScreenLockService.isRunning()){
-                        lockService.setAction("com.example.fetch1.ScreenLockService");
-                        startService(lockService);
+                        Log.d(TAG, "Starting Screen Lock Service");
+                        SLS.setAction("com.example.fetch1.ScreenLockService");
+                        SLS.putExtra("FLASH", flash);
+                        SLS.putExtra("SENSITIVITY", sensitivity);
+                        startService(SLS);
                     }
                 }
                 else{
                     //Stop the Screen Lock Service
-                    Intent lockService = new Intent(this, ScreenLockService.class);
-                    if(ScreenLockService.isRunning()){
-                        stopService(lockService);
+
+                    ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+                    List<ActivityManager.RunningAppProcessInfo> runningAppProcesses = am.getRunningAppProcesses();
+
+                    Iterator<ActivityManager.RunningAppProcessInfo> iterate = runningAppProcesses.iterator();
+
+                    while(iterate.hasNext()){
+                        ActivityManager.RunningAppProcessInfo next = iterate.next();
+
+                        String processName = getPackageName() + ":ScreenLockService";
+
+                        if(next.processName.equals(processName)){
+                            Process.killProcess(next.pid);
+                            break;
+                        }
                     }
+
+                    /*Intent SLS = new Intent(this, ScreenLockService.class);
+                    if(ScreenLockService.isRunning()){
+                        Log.d(TAG, "Stopping Screen Lock Service");
+                        SLS.setAction("com.example.fetch1.ScreenLockService");
+                        SLS.putExtra("FLASH", flash);
+                        SLS.putExtra("SENSITIVITY", sensitivity);
+                        stopService(SLS);
+                    }*/
                 }
                 break;
             //the flash light check box *does NOT work - check issues on github
             case R.id.flashBox:
                 if (checked){
                     //set up new intent with flash
-                    Intent SLS = new Intent(ScreenLockService.class.getName());
-                    SLS.putExtra("FLASH", true);
-                    Home.this.startService(SLS);
+                    flash = true;
+                    Context context = getApplicationContext();
+                    Intent SLS = new Intent(context, ScreenLockService.class);
+                    SLS.putExtra("FLASH", flash);
+                    SLS.putExtra("SENSITIVITY", sensitivity);
+                    context.startService(SLS);
+                    Log.d(TAG, "Flash is on");
                 }
                 else {
                     //set up new intent with no flash
-                    Intent SLS = new Intent(ScreenLockService.class.getName());
-                    SLS.putExtra("FLASH", false);
-                    Home.this.startService(SLS);
+                    flash = false;
+                    Context context = getApplicationContext();
+                    Intent SLS = new Intent(context, ScreenLockService.class);
+                    SLS.putExtra("FLASH", flash);
+                    SLS.putExtra("SENSITIVITY", sensitivity);
+                    context.startService(SLS);
+                    Log.d(TAG, "Flash is off");
                 }
                 break;
         }
     }
-    
-    public void onInfoPageClicked(View view){
-        Intent intent = new Intent(this, InfoPage.class);
-        startActivity(intent);
-        //start infoPage activity
-    }
-}
-nt;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.SeekBar;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-public class Home extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        Intent intent = new Intent(this, ScreenLockService.class);
-        intent.putExtra("FLASH", true);
-        intent.putExtra("SENSITIVITY", 0);
-        startService(intent);
-
-        SeekBar seekbar;
-        seekbar = (SeekBar) findViewById(R.id.Sensitivity);
-        TextView sensitivityText;
-        sensitivityText = (TextView) findViewById(R.id.SensitivityText);
-
-        sensitivityText.setText("Sensitivity:" + seekbar.getProgress());
-
-        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int finalProgress = 0;
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                finalProgress = progress;
-                //Start screenLockService with new sensitivity setting
-                Intent SLS = new Intent(ScreenLockService.class.getName());
-                SLS.putExtra("SENSITIVITY", finalProgress);
-                Home.this.startService(SLS);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-    }
-
-    public void onHomeBoxChecked(View view){
-
-        //Get if the box was checked or unchecked
-        boolean checked = ((CheckBox) view).isChecked();
-
-        //Get which box was clicked (flash or on/off)
-        switch(view.getId()) {
-            case R.id.appOnBox:
-                if (checked){
-                    //Start the Screen Lock Service
-                    Intent lockService = new Intent(this, ScreenLockService.class);
-                    if(!ScreenLockService.isRunning()){
-                        lockService.setAction("com.example.fetch1.ScreenLockService");
-                        startService(lockService);
-                    }
-                }
-                else{
-                    //Stop the Screen Lock Service
-                    Intent lockService = new Intent(this, ScreenLockService.class);
-                    if(ScreenLockService.isRunning()){
-                        stopService(lockService);
-                    }
-                }
-                break;
-            case R.id.flashBox:
-                if (checked){
-                    //Enable info.flash
-                    Intent SLS = new Intent(ScreenLockService.class.getName());
-                    SLS.putExtra("FLASH", true);
-                    Home.this.startService(SLS);
-                }
-                else {
-                    //Disable the flash
-                    Intent SLS = new Intent(ScreenLockService.class.getName());
-                    SLS.putExtra("FLASH", false);
-                    Home.this.startService(SLS);
-                }
-                break;
-        }
-    }
-
 
     public void onInfoPageClicked(View view){
         Intent intent = new Intent(this, InfoPage.class);
         startActivity(intent);
         //start infoPage activity
     }
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
 }
